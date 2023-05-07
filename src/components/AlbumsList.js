6cfc8abebe8e2cc42e8e10a539f2efa241acd234
sayedhany <sayedh@mail.ru>
2023-05-07 10:06:43 +0300

    added

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const AlbumsList = ({user}) => {
-  return <div>Albums for {user.name}</div>;
+const AlbumsList = ({ user }) => {
+  return <div>Albums for the {user.name}</div>;
 };
 
 export default AlbumsList;
